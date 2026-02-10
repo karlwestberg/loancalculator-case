bd101b2771027ce0fd63a063b0cf0c39dc3cce52
karlwestberg <westbergkarl@gmail.com>
2026-02-10 16:16:51 +0100

    Minor tweaks

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ const queryClient = new QueryClient()
 export default function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <div className='w-full max-w-96 min-w-24 p-2'>
+      <div className='w-full max-w-96 min-w-24 p-2 mt-5'>
         <div className='mb-4'>
           <h1 className='font-bold'>Frauda</h1>
           <p>We are legit, we promise</p>
